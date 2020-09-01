36957b58cbd10c406055c888dfbbf446a58305b8
DizdarArmin <armin.dizdar@gmail.com>
2020-09-01 11:18:43 +0200

    JS and WebApi

--- Startup.cs
+++ Startup.cs
@@ -24,11 +24,16 @@ namespace WebApi_
 
         public void ConfigureServices(IServiceCollection services) 
         {
+            services.AddCors();
             services.AddControllers();
         }
 
         public void Configure(IApplicationBuilder app, IWebHostEnvironment env) 
         {
+
+            app.UseCors(
+                options => options.WithOrigins("*").AllowAnyMethod()
+                );
             app.UseRouting();
             app.UseAuthorization();
             app.UseEndpoints(endpoints =>
