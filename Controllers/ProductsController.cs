dd2a230d56abcedb11be4e009771e4cfbb2e4fcb
DizdarArmin <armin.dizdar@gmail.com>
2020-09-01 09:14:10 +0200

    Crud

--- Controllers/ProductsController.cs
+++ Controllers/ProductsController.cs
@@ -12,7 +12,7 @@ namespace WebApi_.Controllers
     [ApiController]
     public class ProductsController : ControllerBase
     {
-        List<Product> products = new List<Product>()
+        private static List<Product> products = new List<Product>()
         {
             new Product{
             Id = 1006368,
@@ -41,11 +41,54 @@ namespace WebApi_.Controllers
         {
             return products;
         }
+
         [HttpGet("{id}")]
-        public Product Get(int id) 
+        public ActionResult<Product> Get(int id) 
         {
             var product = products.Find(product => product.Id == id);
+            if (product == null) 
+            {
+                return NotFound();
+            }
             return product;
         }
+
+        [HttpPost]
+        public ActionResult Post([FromBody] Product product) 
+        {
+            if (products.Exists(p =>p.Id == product.Id)) 
+            {
+                return Conflict();
+            }
+            products.Add(product);
+            return CreatedAtAction(nameof(Get), new { id = product.Id }, products);
+        }
+
+        [HttpDelete("{id}")]
+        public ActionResult<IEnumerable<Product>> Delete(int id)
+        {
+            var product = products.Where(product => product.Id == id);
+            if (product == null) 
+            {
+                return NotFound();
+            }
+            products = products.Except(product).ToList();
+            return products;
+        }
+
+        [HttpPut]
+        public ActionResult<IEnumerable<Product>> Put(int id, [FromBody] Product product) 
+        {
+            if(id != product.Id) 
+            {
+                return BadRequest();
+            }
+
+            var existingProduct = products.Where(p => p.Id == id);
+            products = products.Except(existingProduct).ToList();
+            products.Add(product);
+            return products;
+        }
+ 
     }
 }
